da72361ec2de937fc2ed64d2adfd2623efe01af0
Fernando <store.nthing@gmail.com>
2023-03-05 22:31:54 -0300

    Navbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import './assets/styles/custom.scss';
 import './App.css';
+import Navbar from 'components/Navbar';
 
 const App = () => {
   return (
     <>
-      <h1>Hello DSCatalog</h1>
+      <Navbar/>
       <h1>Hello DSCatalog</h1>
     </>
     
